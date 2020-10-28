19df028c9ae6695ec2f926e01d0741b5e327f99e
Kristopher Tokarz <kj.tokarz@gmail.com>
2020-10-28 17:07:35 -0400

    Successfully animate a spritesheet on screen

--- src/characters.rs
+++ src/characters.rs
@@ -1,10 +1,32 @@
 use bevy::prelude::*;
 
-pub struct Person;
-
-pub struct Name(String);
-
-pub fn add_people(mut commands : Commands) {
+pub fn setup_character_sprite_sheet(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("assets/Character/sprites/character-idle-spritesheet.png").unwrap();
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 4, 3);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    
     commands
-        .spawn((Person, Name("Kris".to_string())));
+        .spawn(Camera2dComponents::default())
+        .spawn(SpriteSheetComponents {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(6.0),
+            ..Default::default()
+        })
+        .with(Timer::from_seconds(0.1, true));
+}
+
+pub fn animate_sprite_system(
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
+) {
+    for (timer, mut sprite, texture_atlas_handle) in &mut query.iter() {
+        if timer.finished {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
+        }
+    }
 }
